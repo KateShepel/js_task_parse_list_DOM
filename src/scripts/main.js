34b40afdf0b691fbd7791f209993c7a782045afe
Kateryna Shepel <MariyaKaterina@gmail.com>
2025-02-10 22:01:17 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const getList = [...document.querySelectorAll('li')];
+const ul = document.querySelector('ul');
+
+function sortList(list) {
+  list.sort((a, b) => {
+    const numA = toNumber(a.dataset.salary);
+    const numB = toNumber(b.dataset.salary);
+
+    return numB - numA;
+  });
+}
+
+function toNumber(str) {
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i])) {
+      result += str[i];
+    }
+  }
+
+  return +result;
+}
+
+sortList(getList);
+
+ul.replaceWith(...getList);
